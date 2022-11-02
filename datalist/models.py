f9b48084c004a086a4c116a98e6e27c8147ceb09
sanjarbek <sanjarsaidov173@gmail.com>
2022-11-02 13:28:07 +0500

    int to char

--- datalist/models.py
+++ datalist/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Username(models.Model):
     name = models.CharField(max_length=100)
-    password = models.IntegerField()
+    password = models.CharField(max_length=50)
 
     def __str__(self):
         return self.name
